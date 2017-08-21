dbd16b11cbff36cab8242bea78a7d81e8c69698f
jcosta <costajlmpp@gmail.com>
2017-08-21 20:56:08 +0100

    create compoment com o comando: ng generate compoment compoment-name --spec=false

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {HelloComponent} from './hello/hello.component';
+import { SimpleComponent } from './simple/simple.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent
+    HelloComponent,
+    SimpleComponent
   ],
   imports: [
     BrowserModule
